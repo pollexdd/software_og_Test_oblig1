9d688985414c543891cb8c697783b9e95e9f2d4c
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2023-08-28 15:35:27 +0200

    1.2.5

--- src/test/java/org/example/LeapYearTest.java
+++ src/test/java/org/example/LeapYearTest.java
@@ -1,34 +1,43 @@
 package org.example;
 
-import org.example.LeapYear;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {2004, 2008, 2012})
-    public void testYearsDivisibleByFourButNotOneHundredAreLeapYears(int year) {
-        assertTrue(LeapYear.isLeapYear(year));
+    @Test
+    public void testYearsDivisibleByFourButNotOneHundredAreLeapYears() {
+        int[] years = {2004, 2008, 2012};
+        for (int year : years) {
+            assertTrue(LeapYear.isLeapYear(year));
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {2000, 2400})
-    public void testYearsDivisibleByFourHundredAreLeapYears(int year) {
-        assertTrue(LeapYear.isLeapYear(year));
+    @Test
+    public void testYearsDivisibleByFourHundredAreLeapYears() {
+        int[] years = {2000, 2400};
+        for (int year : years) {
+            assertTrue(LeapYear.isLeapYear(year));
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {2005, 2010, 2022})
-    public void testYearsNotDivisibleByFourAreNotLeapYears(int year) {
-        assertFalse(LeapYear.isLeapYear(year));
+
+    @Test
+    public void testYearsNotDivisibleByFourAreNotLeapYears() {
+        int[] years = {2005, 2010, 2022};
+        for (int year : years) {
+            assertFalse(LeapYear.isLeapYear(year));
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1900, 2100})
-    public void testYearsDivisibleByOneHundredAndNotByFourHundredAreNotLeapYears(int year) {
-        assertFalse(LeapYear.isLeapYear(year));
+    @Test
+    public void testYearsDivisibleByOneHundredAndNotByFourHundredAreNotLeapYears() {
+        int[] years = {1900, 2100};
+        for (int year : years) {
+            assertFalse(LeapYear.isLeapYear(year));
+        }
     }
 }
+
