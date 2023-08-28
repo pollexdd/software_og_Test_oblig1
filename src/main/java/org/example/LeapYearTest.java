9d2ac7e9be5630bd70f23bcaac2d8eea03351ea3
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2023-08-28 15:20:34 +0200

    1.2.5

--- src/main/java/org/example/LeapYearTest.java
+++ src/main/java/org/example/LeapYearTest.java
@@ -1,5 +1,6 @@
 package org.example;
-import org.junit.Test;
+
+import org.example.LeapYear;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,35 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
 
-    @Test
     @ParameterizedTest
     @ValueSource(ints = {2004, 2008, 2012})
     public void testYearsDivisibleByFourButNotOneHundredAreLeapYears(int year) {
         assertTrue(LeapYear.isLeapYear(year));
     }
-    // tests if the year is divisible by 4 but not by 100.
 
-    @Test
     @ParameterizedTest
     @ValueSource(ints = {2000, 2400})
     public void testYearsDivisibleByFourHundredAreLeapYears(int year) {
         assertTrue(LeapYear.isLeapYear(year));
     }
-    // tests if the year is divisible by 400.
 
-    @Test
     @ParameterizedTest
     @ValueSource(ints = {2005, 2010, 2022})
     public void testYearsNotDivisibleByFourAreNotLeapYears(int year) {
         assertFalse(LeapYear.isLeapYear(year));
     }
-    // tests if the year is not divisible by 4.
 
-    @Test
     @ParameterizedTest
     @ValueSource(ints = {1900, 2100})
-    public void testYearsDivisibleByOnehundredAndNotByFourHundredAreNotLeapYears(int year) {
+    public void testYearsDivisibleByOneHundredAndNotByFourHundredAreNotLeapYears(int year) {
         assertFalse(LeapYear.isLeapYear(year));
     }
-    // tests if the year is divisible by 100 but not by 400.22
 }
