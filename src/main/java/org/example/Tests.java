719037477eea776386e3a19fc6d3efe57c9315de
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2023-08-28 15:12:46 +0200

    1.2.5

--- src/main/java/org/example/Tests.java
+++ src/main/java/org/example/Tests.java
@@ -1,5 +1,5 @@
 package org.example;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertFalse;
